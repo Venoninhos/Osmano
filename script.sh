a8e9a3eebd9156fc11a62d8ee986ea1906a39b3d
Yan <yanmv11@gmail.com>
2017-11-22 12:07:16 -0200

    versão 2.0
    github.com/valvesss

--- script.sh
+++ script.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
-A=0
-B=0
-while [ $A -lt 2 ]
-do
-	CPU=$(top -d 1 -b -n2 | grep "Cpu(s)"|tail -n 1 | awk '{print $2 + $4}')
-	if [ $CPU -gt 80 ]; then
-		B=1
-	else
-		B=0
-	fi
-if [ $B -eq 1 ]; then
-        B=0
-        xterm -e dialog --msgbox "Aviso: uso da CPU acima de 80% ($CPU%)" 10 20
-        sleep 10
+root2=$(whoami)
+isroot=0
+if [ $root2 = root ]; then
+isroot=1
+dialog --msgbox "Seu usuário é ROOT. Você terá permissões especiais." 10 20
 fi
-done &
+B=0
+disk(){
+	DISK=$(df -hT | grep /dev/ | awk '{print $6}' | sed 's/[%]$//')
+	if [ $DISK -gt 50 ]; then
+		dialog --msgbox "Uso de disco acima de 50% ($DISK%)" 10 20
+	fi
+}
+ram(){
+	MEM=$(free -m | grep Mem | awk '{printf "%0.f\n", $3 / $2 * 100}')
+        if [ $MEM -gt 1 ]; then
+        	dialog --msgbox "Memória acima de 70% ($MEM%)" 10 10 
+        fi
+}
+cpu(){
+	CPU=$(top -d 1 -b -n2 | grep "Cpu(s)"|tail -n 1 | awk '{print $2 + $4}')
+	if [ $CPU -gt 70 ]; then
+		dialog --msgbox "Aviso: uso da CPU acima de 70% ($CPU%)" 10 20
+	fi
+}
+menu(){
 clear
 cmd=(dialog --title "Gerenciamento do computador" --menu "Selecione as opções:" 13 30 14)
 options=(1 "CPU" 
          2 "Memória" 
          3 "Disco" 
-         4 "Sair" )
+         4 "Processos" 
+	 5 "Sair" )
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
 for choice in $choices
 do
     case $choice in
         1)
+	    cpu
             export NMON=c
-            nmon
+            nmon; menu
             ;;
         2)
+	    ram
             export NMON=m
-            nmon
+            nmon; menu
             ;;
         3)
+	    disk
             export NMON=d
-            nmon 
+            nmon; menu
             ;;
         4)
-
+	    
+	    ps aux
             ;;
+	5)
+	    
+	    ;;
     esac
 done
+}
+menu
+exit 0
